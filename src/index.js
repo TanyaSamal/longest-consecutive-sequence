93a12406e2b2d4b0c9b3245dcbe77baca9da3ab0
TanyaSamal <solesytto@gmail.com>
2018-09-24 23:10:20 +0300

    resolve longest consecutive sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+  if (array.length == 0) return 0;
+
+  if (array.length == 1) return 1;
+
+  let maxLength = 1;
+
+  let hashTable = new Set();
+
+  for (let key in array) {
+    hashTable.add(array[key]);
+  }
+
+  for (let i = 0, len = array.length; i < len; i++){
+    let values = 1;
+
+    let lNeighbor = array[i] - 1;
+
+    while(hashTable.has(lNeighbor)){
+        hashTable.delete(lNeighbor);
+        values++;
+        lNeighbor = lNeighbor - 1;
+    }
+
+    if (values > maxLength) maxLength = values;
+
+    let rNeighbor = array[i] + 1;
+
+    while(hashTable.has(rNeighbor)){
+        hashTable.delete(rNeighbor);
+        values++;
+        rNeighbor++;
+    }
+
+    if (values > maxLength) maxLength = values;
+}
+
+  return maxLength;
 }
